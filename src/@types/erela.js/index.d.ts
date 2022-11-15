2a09fe7a34b276bc2b5628e65002a598ad1d0fae
pedrx07 <pedrx07@outlook.com>
2022-11-15 10:33:07 -0300

    Remove Type PlayerOptions

--- src/@types/erela.js/index.d.ts
+++ src/@types/erela.js/index.d.ts
@@ -1,9 +1,6 @@
 import Erela from 'erela.js';
 
 declare module 'erela.js' {
-  export interface PlayerOptions {
-    messageId?: string;
-  }
   export interface Player {
     messageId?: string;
   }
