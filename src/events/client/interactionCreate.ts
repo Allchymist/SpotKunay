023bd2507135b0308e4b334a2cd1ca5ec56a6cd2
pedrx07 <pedrx07@outlook.com>
2023-01-09 20:00:49 -0300

    Update Interaction to Database Guild and User

--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -12,18 +12,32 @@ export default class InteractionCreate {
 
   async execute(client: Client<true>, interaction: Interaction) {
     const target = interaction.member as GuildMember;
+    const Member = await client.Database.user.findById(target.id);
+    if (!Member) {
+      await new client.Database.user({
+        _id: target.id,
+        tag: target.user.tag,
+        username: target.user.username,
+        avatar:target.displayAvatarURL(),
+        playlists: []
+      }).save();
+    }
 
     if (interaction.isChatInputCommand()) {
+      await interaction.deferReply({
+        fetchReply: true,
+        ephemeral: true,
+      });
+      
       const command = client.Commands.get(interaction.commandName);
-
       if (command.inVoiceChannel && !target.voice.channelId) {
-        return interaction.reply('Você precisa estar em um canal de voz.');
+        return interaction.editReply('Você precisa estar em um canal de voz.');
       }
 
       if (command.cooldown) {
         if (client.Cooldowns.has(`${interaction.user.id}-${interaction.commandName}`)) {
           const time = moment.duration(client.Cooldowns.get(`${interaction.user.id}-${interaction.commandName}`) - Date.now()).format('m [minutos] s [segundos]');
-          return interaction.reply(`Você precisa esperar ${time} para usar este comando novamente.`);
+          return interaction.editReply(`Você precisa esperar ${time} para usar este comando novamente.`);
         } else {
           client.Cooldowns.set(`${command.data.name}-${interaction.user.id}`, Date.now() + (command.cooldown * 1000));
           setTimeout(() => client.Cooldowns.delete(`${command.data.name}-${interaction.user.id}`), command.cooldown * 1000);
@@ -31,10 +45,22 @@ export default class InteractionCreate {
       }
 
       try {
-        await interaction.deferReply({
-          ephemeral: command.ephemeral,
-          fetchReply: true,
-        });
+        let Guild = await client.Database.guild.findById(interaction.guildId);
+        if (!Guild) {
+          Guild = await new client.Database.guild({
+            _id: interaction.guildId,
+            isPremium: false
+          }).save();
+        }
+
+        if (command.isPremium && !Guild.isPremium) {
+          return interaction.editReply({
+            embeds: [{
+              color: 0xFF0000,
+              description: 'Este servidor não é premium'
+            }]
+          });
+        }
 
         await command.execute(client, interaction);
       } catch (err) {
@@ -42,6 +68,16 @@ export default class InteractionCreate {
         console.error(message);
         
         return interaction.editReply(`Ocorreu um erro ao executar este comando.`)
+      }
+    }
+
+    if (interaction.isAutocomplete()) {
+      const command = client.Commands.get(interaction.commandName);
+
+      try {
+        await command.autoComplete(client, interaction);
+      } catch (error) {
+        console.error(error);
       }
     }
   }
