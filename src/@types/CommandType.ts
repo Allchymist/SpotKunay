6108923f7c0c718ee00a2fb99b287db85dae6d74
pedrx07 <pedrx07@outlook.com>
2023-01-09 19:59:56 -0300

    Add type isPremium in Commands

--- src/@types/CommandType.ts
+++ src/@types/CommandType.ts
@@ -1,10 +1,11 @@
-import { ApplicationCommandData, CommandInteraction, Client } from 'discord.js';
+import { ApplicationCommandData, CommandInteraction, Client, AutocompleteInteraction } from 'discord.js';
 
 export type CommandType = {
   data: ApplicationCommandData;
-  ephemeral: boolean;
   cooldown: number;
   inVoiceChannel?: boolean;
+  isPremium: boolean;
 
   execute(client: Client, int: CommandInteraction): any | Promise<any>;
+  autoComplete?(client: Client, interaction: AutocompleteInteraction): any | Promise<any>;
 }
